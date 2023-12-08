a739762f76df97484ea0c22abe08f30146f7cb12
deitschs <deitschs@berea.edu>
2023-12-07 20:18:58 -0500

    some updates and better organization

--- codebase/myscripts.js
+++ codebase/myscripts.js
@@ -1,4 +1,7 @@
-﻿function allowDrop(ev) {
+﻿//code for drag and drop
+//credit https://www.w3schools.com/html/html5_draganddrop.asp
+
+function allowDrop(ev) {
     ev.preventDefault();
 }
 
@@ -10,4 +13,59 @@ function drop(ev) {
     ev.preventDefault();
     var data = ev.dataTransfer.getData("text");
     ev.target.appendChild(document.getElementById(data));
+}
+
+
+//code for popovers
+//credit https://www.w3schools.com/bootstrap/bootstrap_popover.asp
+//also https://codepen.io/admixarts/pen/GXRbwM 
+
+$(function () {
+
+    $("[data-toggle=popover]").popover({
+        html: true,
+        content: function () {
+            var content = $(this).attr("data-popover-content");
+            return $(content).children(".popover-body").html();
+        },
+        title: function () {
+            var title = $(this).attr("data-popover-content");
+            return $(title).children(".popover-heading").html();
+        }
+    });
+
+});
+
+$('.popover-dismiss').popover({
+    trigger: 'focus'
+})
+
+//code for image slideshow
+//credit https://www.w3schools.com/w3css/w3css_slideshow.asp
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+    showDivs(slideIndex += n);
+}
+
+function currentDiv(n) {
+    showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("demo");
+    if (n > x.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = x.length }
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" w3-white", "");
+    }
+    x[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " w3-white";
 }
